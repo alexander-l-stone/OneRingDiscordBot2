8403c89c8448cc61228ce564553f7e34abd872c8
Richard Nicholson <twilly0@gmail.com>
2022-02-24 19:09:49 -0800

    Changed printing to have rich text

--- one_ring.py
+++ one_ring.py
@@ -18,7 +18,7 @@ class OneRingCommands(commands.Cog):
         for i in range(0, num_dice):
             d6 = randint(1, 6)
             if d6 < 4:
-                d6 = 0
+                d6 *= -1
             die_array.append(d6)
         return die_array
     
@@ -26,72 +26,58 @@ class OneRingCommands(commands.Cog):
         """
             Rolls a feat die. If favored is < 1, this roll is unfavored.
             If favored is > 1, roll is favored.
+            Returns a "bad" symbol as negative
         """
         original_roll = randint(1, 12)
         if original_roll == 11:
-            original_roll = 0
+            original_roll *= -1
         if favored != 0:
             second_roll = randint(1, 12)
             if second_roll == 11:
-                second_roll = 0
+                second_roll *= -1
         if favored > 0:
             print(f"Original Roll: {original_roll}, Second Roll: {second_roll}")
-            return max(original_roll, second_roll)
+            return (max(original_roll, second_roll), min(original_roll, second_roll)) 
         if favored < 0:
             print(f"Original Roll: {original_roll}, Second Roll: {second_roll}")
-            return min(original_roll, second_roll)
-        return original_roll
+            return (min(original_roll, second_roll), max(original_roll, second_roll)) 
+        return (original_roll)
     
     def get_gm_feat_roll(self, favored: int):
         """
             Rolls a feat die. If favored is < 1, this roll is unfavored.
             If favored is > 1, roll is favored.
+            Returns a "bad" symbol as negative
         """
         original_roll = randint(1, 12)
         if original_roll == 12:
-            original_roll = 0
+            original_roll *= -1
         if favored != 0:
             second_roll = randint(1, 12)
             if second_roll == 12:
-                second_roll = 0
+                second_roll *= -1
         if favored > 0:
             print(f"Original Roll: {original_roll}, Second Roll: {second_roll}")
-            return max(original_roll, second_roll)
+            return (max(original_roll, second_roll), min(original_roll, second_roll)) 
         if favored < 0:
             print(f"Original Roll: {original_roll}, Second Roll: {second_roll}")
-            return min(original_roll, second_roll)
-        return original_roll
+            return (min(original_roll, second_roll), max(original_roll, second_roll)) 
+        return (original_roll)
 
-    def generate_player_text_box(self, skill_roll, feat_die):
-        total = 0
-        num_sixes = 0
-        dice_string = ""
-        for num in skill_roll:
-            total += num
-            dice_string = f"{dice_string}{num} "
-            if num == 6:
-                num_sixes += 1
-        total += feat_die
-        embed = Embed(title=f"Total: {total}", description=f"{dice_string} | {feat_die}", colour=0x0000FF)
-        if feat_die == 12:
+    def generate_player_text_box(self, skill_roll:str, feat_roll:tuple(int,int)):
+        rollData = self.parse_roll(skill_roll, feat_roll)
+        embed = Embed(title=f"Total: {rollData[0]}", description=f"{rollData[1]}", colur=0xFF0000)
+        if rollData[3] > 0:
             embed.add_field(name='Automatic Success', value='You rolled a Gandalf Rune', inline=False)
-        embed.add_field(name='Number of Sixes', value=f"{num_sixes}", inline=False)
+        embed.add_field(name='Number of Sixes', value=f"{rollData[2]}", inline=False)
         return embed
     
-    def generate_gm_text_box(self, skill_roll, feat_die):
-        total = 0
-        num_sixes = 0
-        dice_string = ""
-        for num in skill_roll:
-            total += num
-            dice_string = f"{dice_string}{num} "
-            if num == 6:
-                num_sixes += 1
-        total += feat_die
-        embed = Embed(title=f"Total: {total}", description=f"{dice_string} | {feat_die}", colur=0xFF0000)
-        if feat_die == 11:
+    def generate_gm_text_box(self, skill_roll:str, feat_roll:tuple(int,int)):
+        rollData = self.parse_roll(skill_roll, feat_roll)
+        embed = Embed(title=f"Total: {rollData[0]}", description=f"{rollData[1]}", colur=0xFF0000)
+        if rollData[3] < 0:
             embed.add_field(name='Automatic Success', value='You rolled an Eye of Sauron', inline=False)
-        embed.add_field(name='Number of Sixes', value=f"{num_sixes}", inline=False)
+        embed.add_field(name='Number of Sixes', value=f"{rollData[2]}", inline=False)
         return embed
 
     def parse_text(self, text: str):
@@ -107,8 +93,72 @@ class OneRingCommands(commands.Cog):
                 commands['weary'] = True
             elif text.isnumeric():
                 commands['num_dice'] += int(text)
+            else:
+                commands['label'] += f"{commands['label']}{text} "
             print(commands)
         return commands
+
+    def parse_roll(self, skill_roll, feat_roll):
+        """
+        Turns a roll into data to be displayed.
+        returns (total, roll_string, num_sixes, specialFeat)
+        """
+        total = 0
+        num_sixes = 0
+        roll_string = ""
+        for num in skill_roll:
+            if num > 0:
+                total += num
+                if num == 6:
+                    # underline sixes
+                    roll_string = f"{roll_string}__{num}__ "
+                else:
+                    # normal value
+                    roll_string = f"{roll_string}{num} "
+            else:
+                # display lost dice from weary crossed out
+                num *= -1
+                roll_string = f"{roll_string}~~{num}~~ "
+            if num == 6:
+                num_sixes += 1
+
+        # feat die
+        roll_string = f"{roll_string}| "
+
+        if feat_roll[0] > 0:
+            total += feat_roll[0]
+            if feat_roll[0] == 12 or feat_roll[0] == 11:
+                # bold special symbol
+                roll_string = f"{roll_string}**{feat_roll[0]}** "
+            else:
+                # normal feat die
+                roll_string = f"{roll_string}{feat_roll[0]} "
+        else:
+            # bad rune is crossed out and bold
+            badFeat = -1*feat_roll[0]
+            roll_string = f"{roll_string}~~**{badFeat}**~~ "
+                
+        # Ignored die, if any
+        if len(feat_roll) > 1:
+            ignoredFeat = feat_roll[1]
+            if ignoredFeat < 0:
+                ignoredFeat *= -1
+            if ignoredFeat == 11 or ignoredFeat == 12:
+                # cross out and bold ignored special symbol
+                roll_string = f"{roll_string}~~**{-1*ignoredFeat}**~~ "
+            else:
+                # cross out ignored feat die
+                roll_string = f"{roll_string}~~{-1*ignoredFeat}~~ "
+        
+        specialFeat = 0
+        if abs(feat_roll[0]) == 11:
+            specialFeat = -1
+        elif abs(feat_roll[0]) == 12:
+            specialFeat = 1
+
+        return (total, roll_string, num_sixes, specialFeat)
+
+
 
     @commands.command(name="roll")
     async def roll(self, ctx: commands.Context, *, text: str):
@@ -120,8 +170,8 @@ class OneRingCommands(commands.Cog):
             skill_roll = self.get_weary_roll(parsed_commands['num_dice'])
         else:
             skill_roll = self.get_skill_roll(parsed_commands['num_dice'])
-        feat_die = self.get_player_feat_roll(parsed_commands['favored'])
-        await ctx.send(embed=self.generate_player_text_box(skill_roll, feat_die))
+        feat_roll = self.get_player_feat_roll(parsed_commands['favored'])
+        await ctx.send(embed=self.generate_player_text_box(skill_roll, feat_roll))
     
     @commands.command(name="gmroll")
     async def gmroll(self, ctx: commands.Context, *, text: str):
